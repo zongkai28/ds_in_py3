8e7a7f3d777cba06fd62a82efec4c8f3cdb929f3
kenneth <zongkai28@qq.com>
2019-09-22 22:44:16 +0800

    final version of 1-04.

--- 0920_algorithm complexity.py
+++ 0920_algorithm complexity.py
@@ -1,7 +1,14 @@
+# python 3.7.3
 import time
 
-start_time = time.time()
+start_time = time.time_ns()
 
 for a in range(0, 1001):
     for b in range(0, 1001):
-        print("a, b:%d, %d"%(a, b))
+        c = 1000 - a - b
+        if a**2 + b**2 == c**2:
+            print("a, b:%d, %d."%(a, b))
+
+end_time = time.time_ns()
+
+print("eclipsed time:%d ms."%((end_time - start_time)/1000000))
